ab6924686058dcd7e9574fec273c990f054b457b
igorgamer001 <sichkoi00@gmail.com>
2025-02-16 21:39:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+
+
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
